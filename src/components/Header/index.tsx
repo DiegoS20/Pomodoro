9360856f2e352e7f40d562586be47b0c0f5fb65a
Diego Saravia <saravia__2000@hotmail.com>
2023-07-21 10:04:34 -0600

    feat: component created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import { Dispatch, SetStateAction } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import styles from "./styles";
+
+export default function Header({
+  currentTime,
+  setCurrentTime,
+  setTime,
+}: HeaderProps) {
+  const handlePress = (option: OptionsType) => {
+    const TIMES = {
+      Pomodoro: 25,
+      "Short Break": 5,
+      "Long Break": 15,
+    } as const;
+    const newTime = TIMES[option];
+    setCurrentTime(option);
+    setTime(newTime * 60);
+  };
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {OPTIONS.map((o, i) => (
+        <TouchableOpacity
+          key={i}
+          style={[
+            styles.itemStyle,
+            currentTime != o && { borderColor: "transparent" },
+          ]}
+          onPress={() => handlePress(o)}
+        >
+          <Text style={{ fontWeight: "bold" }}>{o}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
+
+export const OPTIONS = ["Pomodoro", "Short Break", "Long Break"] as const;
+
+type OptionsType = (typeof OPTIONS)[number];
+type HeaderProps = {
+  currentTime: OptionsType;
+  setCurrentTime: Dispatch<SetStateAction<OptionsType>>;
+  setTime: Dispatch<SetStateAction<number>>;
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  itemStyle: {
+    width: "33%",
+    borderWidth: 3,
+    padding: 5,
+    borderColor: "#fff",
+    marginVertical: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+});
